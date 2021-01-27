3741dce92640829f8c538b0a20021e6085e48249
James Perkins <jamesperkins@hey.com>
2021-01-27 08:59:03 -0500

    Adding all files

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,39 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Container from "@/components/ContainerComponent";
+import { Heading, Text, Button, Stack, Box } from "@chakra-ui/react";
+import { useAuth } from "@nhost/react-auth";
 
 export default function Home() {
+  const { signedIn } = useAuth();
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    <Container>
+      <Heading as="h1" textAlign="center">
+        Welcome to our Quiz!
+      </Heading>
+      <Box mt={8} mx="auto">
+        <Text textAlign="center">
+          Welcome to our quiz app! To get started click a button below.
+        </Text>
+        {!signedIn && (
+          <Stack justifyContent="center" isInline mt={6}>
+            <Button width="25%" as="a" href="/signup">
+              Signup
+            </Button>
+            <Button width="25%" as="a" href="/login">
+              Login
+            </Button>
+          </Stack>
+        )}
+        {signedIn && (
+          <Stack justifyContent="center" isInline mt={6}>
+            <Button width="25%" as="a" href="/create">
+              Create a question
+            </Button>
+            <Button width="25%" as="a" href="/quiz">
+              Take a quiz
+            </Button>
+          </Stack>
+        )}
+      </Box>
+    </Container>
+  );
 }
